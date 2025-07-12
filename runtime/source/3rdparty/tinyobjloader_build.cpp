43686c3219bdcd35bd75371039fc6042e7641f43
Ziyi Lu <52308484+LuniumLuk@users.noreply.github.com>
2025-07-13 06:39:52 +0800

    Add tinyobjloader library to 3rdparty
    Added the tinyobjloader header, license, and build source files to the 3rdparty directory for OBJ file parsing support.

---
+++ runtime/source/3rdparty/tinyobjloader_build.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+// Optional. define TINYOBJLOADER_USE_MAPBOX_EARCUT gives robust triangulation. Requires C++11
+//#define TINYOBJLOADER_USE_MAPBOX_EARCUT
+#include "tinyobjloader/tiny_obj_loader.h"
