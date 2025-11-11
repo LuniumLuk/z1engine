6b39166b2898cada2007280df847004ff061df07
Ziyi Lu <52308484+LuniumLuk@users.noreply.github.com>
2025-11-11 20:12:57 +0800

    Add build and generation scripts for Visual Studio 2022; set up release folder structure and file copying

--- build_vs2022.bat
+++ build_vs2022.bat
@@ -1,2 +1,12 @@
+set VS_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com"
 call utils\premake\premake5.exe vs2022
-pause
+call %VS_PATH% z1engine.sln /Build "Release|x64"
+if exist "release" (
+    echo Deleting existing release folder...
+    rmdir /s /q "release"
+)
+mkdir "release"
+mkdir "release\editor"
+xcopy "engine" "release\engine" /E /I /H /Y
+copy "build\Release-windows-x86_64\editor\editor.exe" "release\editor.exe" /Y
+copy "editor\default.ini" "release\editor\default.ini" /Y
